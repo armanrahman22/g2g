d694103fc7ff7720c13c49f3bf50ed34fe792d08
Arman <armanrahman22@gmail.com>
2016-04-30 22:07:10 -0400

    Fixed most of the CSS

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Arman on 4/29/2016.
+ */
+$(document).on('click', '.number-spinner button', function () {
+    var btn = $(this),
+        oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+        newVal = 0;
+
+    if (btn.attr('data-dir') == 'up') {
+        newVal = parseInt(oldValue) + 1;
+    } else {
+        if (oldValue > 1) {
+            newVal = parseInt(oldValue) - 1;
+        } else {
+            newVal = 1;
+        }
+    }
+    btn.closest('.number-spinner').find('input').val(newVal);
+});
